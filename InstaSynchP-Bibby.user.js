74b256792df04847abcb5c69fe4353bc34a77260
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-02-22 04:55:17 +0100

    Added red colors for too long walls Some refactoring

--- InstaSynchP-Bibby.user.js
+++ InstaSynchP-Bibby.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Bibby specific changes
 
-// @version     1.0.2
+// @version     1.0.3
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Bibby
 // @license     MIT
@@ -29,33 +29,79 @@ function Bibby(version) {
   }];
 }
 
-Bibby.prototype.wallcounterNotification = function (video) {
-  var wallcounter = window.plugins.wallcounter,
-    message, counter;
+Bibby.prototype.wallcounterNotificationOnce = function () {
+  "use strict";
+  var th = this,
+    wallcounter = window.plugins.wallcounter;
   if (isUdef(wallcounter)) {
+    return;
+  }
+  wallcounter.formatOutput = function (counts) {
+    var output = "Wallcounter<br>";
+    counts.forEach(function (count, index) {
+      var text = "{0}[<b>{2}</b> - {1}] - ";
+      if (count.duration > 60 * 60) {
+        text = '<span style="color:red">{0}[<b>{2}</b> - {1}]</span> - ';
+      }
+      output += text.format(
+        count.origName,
+        count.count,
+        utils.secondsToTime(count.duration)
+      );
+      //2 counters per line
+      if ((index + 1) % 2 === 0) {
+        //remove " - "
+        output = output.substring(0, output.length - 3);
+        output += '<br>';
+      }
+    });
+    //remove " - "
+    if (counts.length % 2 === 1) {
+      output = output.substring(0, output.length - 3);
+    }
+    return output;
+  };
+};
+
+Bibby.prototype.wallcounterNotification = function () {
+  "use strict";
+  var th = this,
+    wallcounter = window.plugins.wallcounter;
+  if (isUdef(wallcounter)) {
+    return;
+  }
+
+  function checkCounter(video) {
+    var message, counter;
     //wallcounter not installed
-    return;
+    counter = wallcounter.counter[video.addedby.toLowerCase()];
+    if (isUdef(counter) || counter.duration < 60 * 60) {
+      //nothing to report
+      return;
+    }
+    message = 'Wallcounter {0}[{1} - {2}]'.format(
+      counter.origName,
+      window.utils.secondsToTime(counter.duration),
+      counter.count);
+    addErrorMessage(message);
   }
-  counter = wallcounter.counter[video.addedby.toLowerCase()];
-  if (isUdef(counter) || counter.duration < 60 * 60) {
-    //nothing to report
-    return;
-  }
-  message = 'Wallcounter {0}[{1} - {2}]'.format(
-    counter.origName,
-    window.utils.secondsToTime(counter.duration),
-    counter.count);
-  addErrorMessage(message);
+
+  events.on(th, 'AddVideo', checkCounter);
+};
+
+Bibby.prototype.executeOnce = function () {
+  "use strict";
+  var th = this;
+  th.wallcounterNotificationOnce();
 };
 
 Bibby.prototype.postConnect = function () {
   "use strict";
   var th = this;
-  if(window.room.roomName.toLowerCase() !== 'bibby'){
+  if (window.room.roomName.toLowerCase() !== 'bibby') {
     return;
   }
-  //add events after we connected so it doesn't spam the chat
-  events.on(th, 'AddVideo', th.wallcounterNotification);
+  th.wallcounterNotification();
 };
 
 
@@ -66,4 +112,4 @@ Bibby.prototype.resetVariables = function () {
 };
 
 window.plugins = window.plugins || {};
-window.plugins.bibby = new Bibby('1.0.2');
+window.plugins.bibby = new Bibby('1.0.3');
